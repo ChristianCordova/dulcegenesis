a9f5ea7d4d32a0cf8ac980b31f753dee0f93e04e
Christian Cordova <crs.cordovam@gmail.com>
2022-04-22 21:43:35 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+const burger = document.querySelector('.header__burger');
+const hnav = document.querySelector('.header__navigation');
+const navlinks = document.querySelectorAll('.menu__link');
+
+burger.addEventListener("click", () => {
+	hnav.classList.toggle('header__navigation--visible');
+});
+/* 
+navlinks.forEach((link)=>{
+	link.addEventListener("click", () => {
+		hnav.classList.remove('header__navigation--visible');
+	});
+})
+
+ */
+
+
+
+navlinks.forEach((link)=>{
+	link.addEventListener("click", () => {
+		hnav.classList.remove('header__navigation--visible');
+	});
+})
+
+
+
+$('#main-carrusel').slick({
+	dots: true,
+	autoplay: true
+});
+
+$('#secondary-carrusel').slick({fade: true});
+
+$('#team-carrusel').slick({
+	dots: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://github.com/elcoach290/html5-manualdeconsulta/blob/master/index.md
